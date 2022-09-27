4f01839ab7675509773a555d5b3fff88bb4b2eb2
Shaheem Naqvi <66822511+ShaheemNaqvi@users.noreply.github.com>
2022-09-27 11:26:53 +0500

    Add files via upload

---
+++ HTML/temp.php
@@ -0,0 +1,17 @@
+
+<?php
+//File to read
+$file = '/sys/bus/w1/devices/28-00299b120204/w1_slave';
+
+//Read the file line by line
+$lines = file($file);
+
+//Get the temp from second line 
+$temp = explode('=', $lines[1]);
+
+//Setup some nice formatting (i.e. 21,3)
+$temp = number_format($temp[1] / 1000, 1, ',', '');
+
+//And echo that temp
+echo $temp . " °C";
+?>
